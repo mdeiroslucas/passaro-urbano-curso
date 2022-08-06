a366bc8b7392a3e524babd17cef3f9489c73f6b0
mdeiroslucas <lucas-medeiros@live.com>
2022-08-06 16:42:00 -0300

    implementing propertybinds

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Oferta } from 'src/app/model/oferta';
+import { OfertasService } from 'src/app/services/ofertas.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  public ofertas: Array<Oferta> = [];
+
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertas = this.ofertasService.getOfertas();
   }
 
 }
