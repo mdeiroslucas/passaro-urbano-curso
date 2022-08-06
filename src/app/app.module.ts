938fb6b21147bce0f785d183d38f5fa8fdaf5cde
mdeiroslucas <lucas-medeiros@live.com>
2022-08-06 15:25:26 -0300

    reuploading files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TopoComponent } from './components/topo/topo.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
